e7f9abfd22d871a221126c98ff8d4cf7128138e4
Moulinmar <marcus.moulin@rpn.ch>
2017-11-10 11:10:13 +0100

    Create the main source, for my skeleton

--- Program.cs
+++ Program.cs
@@ -10,6 +10,53 @@ namespace Calculatrice
     {
         static void Main(string[] args)
         {
+            double dblVal1,dblVal2,dblRep=0;
+            bool isOpIncorrect = true;
+
+            Console.WriteLine("Calculatrice");
+            Console.WriteLine("------------");
+
+            Console.WriteLine("Saisir la première valeur");
+             while(!double.TryParse(Console.ReadLine(), out dblVal1))
+            {
+                Console.Write("Saisir à nouveau la première valeur :");
+            }
+
+            Console.WriteLine("Saisir la deuxième valeur");
+            while (!double.TryParse(Console.ReadLine(), out dblVal2))
+            {
+                Console.Write("Saisir à nouveau la deuxième valeur :");
+            }
+
+            Console.Write("Opération à choix [+ - * / ^]");
+            while (isOpIncorrect)
+            {
+
+                string strOp = Console.ReadLine();
+                isOpIncorrect = false;
+                switch (strOp)
+                {
+                    default:
+                        Console.WriteLine("Opération non reconnue par le système");
+                        Console.WriteLine("Veuillez saisir à nouveau l'opérateur [+ - * / ^]");
+                        break;
+                    case "+":
+                        dblRep = 0;
+                        break;
+                    case "-":
+                        dblRep = 0;
+                        break;
+                    case "*":
+                        dblRep = 0;
+                        break;
+                    case "/":
+                        dblRep = 0;
+                        break;
+                }
+            }
+            Console.Write(dblVal1.ToString());
+
+            Console.ReadKey();
         }
     }
 }
